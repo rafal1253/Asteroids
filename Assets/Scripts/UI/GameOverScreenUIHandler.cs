cb17d0c9aabf596ccf5c7ab0bb946c1f8087cad2
rafal1253 <rafal1253@gmail.com>
2023-04-29 00:43:39 +0200

    Update GameOverScreen appearance

---
+++ Assets/Scripts/UI/GameOverScreenUIHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreenUIHandler : MonoBehaviour
+{
+    [SerializeField] Text _playerNameText;
+    [SerializeField] Text _levelReachedText;
+    [SerializeField] Text _pointsText;
+
+    private void Start()
+    {
+        
+    }
+}
