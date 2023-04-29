e8b4290f35b2083d75fbdb164f9681a8c04c396b
rafal1253 <rafal1253@gmail.com>
2023-04-29 02:22:45 +0200

    Add BackToMenu Button in GameOverScreen

--- Assets/Scripts/UI/GameOverScreenUIHandler.cs
+++ Assets/Scripts/UI/GameOverScreenUIHandler.cs
@@ -8,11 +8,26 @@ public class GameOverScreenUIHandler : MonoBehaviour
     [SerializeField] Text _playerNameText;
     [SerializeField] Text _levelReachedText;
     [SerializeField] Text _pointsText;
+    [SerializeField] Button _backToMenuButton;
 
+    private void OnEnable()
+    {
+        _backToMenuButton.onClick.AddListener(BackToMenu);
+    }
+    private void OnDisable()
+    {
+        _backToMenuButton.onClick.RemoveAllListeners();
+
+    }
     private void Start()
     {
         _playerNameText.text = Data.PlayerName;
         _levelReachedText.text = $"LEVEL {Data.ReachedLevel}";
         _pointsText.text = $"{Data.EarnedPoints}";
+    }
+
+    private void BackToMenu()
+    {
+        SceneLoader.Instance.LoadMainMenu();
     }
 }
