9b461110f6b7896216bd75c3ba3773c391d339e0
rafal1253 <rafal1253@gmail.com>
2023-04-29 02:12:44 +0200

    Modify name validation in MainMenu

--- Assets/Scripts/UI/MainMenuUIHandler.cs
+++ Assets/Scripts/UI/MainMenuUIHandler.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class MainMenuUIHandler : MonoBehaviour
 {
     [SerializeField] InputField _nameInputField;
     [SerializeField] Button _startGameButton;
     [SerializeField] Button _quitGameButton;
+
+    [SerializeField] Color _validationColor;
 
     private void OnEnable()
     {
@@ -21,16 +24,24 @@ public class MainMenuUIHandler : MonoBehaviour
         _startGameButton.onClick.RemoveAllListeners();
         _quitGameButton.onClick.RemoveAllListeners();
     }
+    private void Start()
+    {
+        if (!string.IsNullOrEmpty(Data.PlayerName))
+            _nameInputField.text = Data.PlayerName;
+    }
     private void OnNameChanged()
     {
         Data.PlayerName = _nameInputField.text;
     }
     private void StartGame()
     {
-        if (!string.IsNullOrEmpty(_nameInputField.text))
+        if (!string.IsNullOrWhiteSpace(_nameInputField.text))
             SceneLoader.Instance.LoadGameplayScreen();
         else
-            _nameInputField.placeholder.color = Color.red;
+        {
+            _nameInputField.targetGraphic.DOColor(_validationColor, 0.25f).SetEase(Ease.InOutSine).SetLoops(2, LoopType.Yoyo);
+            _nameInputField.text = null;
+        }
     }
 
     private void QuitGame()
