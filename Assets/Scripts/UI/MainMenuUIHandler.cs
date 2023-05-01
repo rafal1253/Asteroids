e6076545a052ab41a448fd26381ad6e4d85c5a65
rafal1253 <rafal1253@gmail.com>
2023-05-01 21:06:07 +0200

    Change NameInputField type to AdvancedInputField

--- Assets/Scripts/UI/MainMenuUIHandler.cs
+++ Assets/Scripts/UI/MainMenuUIHandler.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using AdvancedInputFieldPlugin;
 
 public class MainMenuUIHandler : MonoBehaviour
 {
-    [SerializeField] InputField _nameInputField;
+    [SerializeField] AdvancedInputField _nameInputField;
     [SerializeField] Button _startGameButton;
     [SerializeField] Button _quitGameButton;
 
@@ -14,33 +15,33 @@ public class MainMenuUIHandler : MonoBehaviour
 
     private void OnEnable()
     {
-        _nameInputField.onValueChanged.AddListener(delegate { OnNameChanged(); });
+        _nameInputField.OnValueChanged.AddListener(delegate { OnNameChanged(); });
         _startGameButton.onClick.AddListener(StartGame);
         _quitGameButton.onClick.AddListener(QuitGame);
     }
     private void OnDisable()
     {
-        _nameInputField.onValueChanged.RemoveAllListeners();
+        _nameInputField.OnValueChanged.RemoveAllListeners();
         _startGameButton.onClick.RemoveAllListeners();
         _quitGameButton.onClick.RemoveAllListeners();
     }
     private void Start()
     {
         if (!string.IsNullOrEmpty(Data.PlayerName))
-            _nameInputField.text = Data.PlayerName;
+            _nameInputField.Text = Data.PlayerName;
     }
     private void OnNameChanged()
     {
-        Data.PlayerName = _nameInputField.text;
+        Data.PlayerName = _nameInputField.Text;
     }
     private void StartGame()
     {
-        if (!string.IsNullOrWhiteSpace(_nameInputField.text))
+        if (!string.IsNullOrWhiteSpace(_nameInputField.Text))
             SceneLoader.Instance.LoadGameplayScreen();
         else
         {
             _nameInputField.targetGraphic.DOColor(_validationColor, 0.25f).SetEase(Ease.InOutSine).SetLoops(2, LoopType.Yoyo);
-            _nameInputField.text = null;
+            _nameInputField.Text = null;
         }
     }
 
